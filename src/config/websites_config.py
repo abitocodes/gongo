5682069f08d0305392d9787dca6ff21e7d8ef936
DSC-LABEL <dogesoundclub@gmail.com>
2024-03-21 17:09:10 +0900

    before coding for selenium

--- src/config/websites_config.py
+++ src/config/websites_config.py
@@ -1,16 +1,16 @@
 websites = [
     {
-        "name": "KISA 한국인터넷진흥원 공지사항",
-        "url": "https://www.kisa.or.kr/401",
+        "name": "KISA 한국인터넷진흥원>알림마당>공지사항",
+        "url": "https://www.kisa.or.kr/401?page=1&searchDiv=30&searchWord=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8&_csrf=e3d30d8e-8d61-4b41-b096-efef607dfcfb",
         "base_url": "https://www.kisa.or.kr",
-        "selector": "tbody tr:not(.notice)",  # 각 게시글의 행 전체를 선택
+        "selector": "tbody tr:not(.notice)", 
         "crawling": "true"
     },
     {
-        "name": "국가과학기술지식정보서비스",
+        "name": "국가과학기술지식정보서비스(NTIS)>국가R&D통합공고",
         "url": "https://www.ntis.go.kr/rndgate/eg/un/ra/mng.do",
         "base_url": "https://www.ntis.go.kr",
-        "selector": ".notice_title",
+        "selector": "table.basic_list tbody tr",
         "crawling": "false"
     },
 ]
--- src/main.py
+++ src/main.py
@@ -1,36 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 import logging
 from .config.websites_config import websites
-from dotenv import load_dotenv
 import os
-import sys
 import csv
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
-
 logging.basicConfig(filename='log.csv', level=logging.INFO, format='%(asctime)s,%(message)s')
-
-slack_token = os.getenv('SLACK_TOKEN')
-slack_channel = os.getenv('SLACK_CHANNEL')
-
-if not slack_channel:
-    print("SLACK_CHANNEL 환경 변수가 설정되어 있지 않습니다. 프로그램을 종료합니다.")
-    sys.exit(1)
-
-client = WebClient(token=slack_token)
 
 def read_log():
     logged_links = []
     try:
-        # log.csv 파일을 열고 csv.reader를 사용하여 읽습니다.
         with open(os.path.join(os.path.dirname(__file__), '..', 'log.csv'), mode='r', newline='', encoding='utf-8') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
-                if row:  # 행이 비어있지 않은 경우에만 처리
-                    link = row[3]  # 네 번째 열(인덱스는 3)이 링크 정보입니다.
+                if row:
+                    link = row[3]
                     logged_links.append(link)
     except FileNotFoundError:
         pass
@@ -51,20 +35,17 @@ def fetch_posts(website):
             posts.append({'title': title, 'link': link, 'date': date, 'source': source})
     return posts
 
-def send_slack_message(posts):
+def log_and_print_posts(posts):
     new_posts = [post for post in posts if post['link'] not in read_log()]
     if not new_posts:
         return False
     for post in new_posts:
-        message = f"일자: {post['date']},제목: {post['title']},출처: {post['source']},링크: {post['link']}"
-        try:
-            client.chat_postMessage(channel=slack_channel, text=message.replace(',', '\n'))
-            # CSV 형식에 맞게 로깅
-            with open(os.path.join(os.path.dirname(__file__), '..', 'log.csv'), 'a', newline='') as log_file:
-                csv_writer = csv.writer(log_file)
-                csv_writer.writerow([post['date'], post['title'], post['source'], post['link']])
-        except SlackApiError as e:
-            logging.error(f"Error sending message: {e.response['error']}")
+        message = f"일자: {post['date']}, 제목: {post['title']}, 출처: {post['source']}, 링크: {post['link']}"
+        print(message.replace(',', '\n'))  # 메시지를 콘솔에 출력합니다.
+        # CSV 형식에 맞게 로깅
+        with open(os.path.join(os.path.dirname(__file__), '..', 'log.csv'), 'a', newline='') as log_file:
+            csv_writer = csv.writer(log_file)
+            csv_writer.writerow([post['date'], post['title'], post['source'], post['link']])
     return True
 
 def main():
@@ -73,7 +54,7 @@ def main():
     for website in websites:
         if website['crawling'] == "true":
             posts = fetch_posts(website)
-            if posts and send_slack_message(posts):
+            if posts and log_and_print_posts(posts):
                 new_posts_found = True
     if not new_posts_found:
         print("새로운 게시글이 없습니다.")
