28b344afb11fa5173012355ff4d7a8f66e68be6b
abitocodes <hancom114@gmail.com>
2024-03-27 17:31:22 +0900

    before adapting headless

--- src/config/websites_config.py
+++ src/config/websites_config.py
@@ -4,13 +4,17 @@ websites = [
         "url": "https://www.kisa.or.kr/401?page=1&searchDiv=30&searchWord=%EB%B8%94%EB%A1%9D%EC%B2%B4%EC%9D%B8&_csrf=e3d30d8e-8d61-4b41-b096-efef607dfcfb",
         "base_url": "https://www.kisa.or.kr",
         "selector": "tbody tr:not(.notice)", 
-        "crawling": "true"
+        "crawling": "true",
+        "selenium": "false",
     },
     {
         "name": "국가과학기술지식정보서비스(NTIS)>국가R&D통합공고",
         "url": "https://www.ntis.go.kr/rndgate/eg/un/ra/mng.do",
         "base_url": "https://www.ntis.go.kr",
         "selector": "table.basic_list tbody tr",
-        "crawling": "false"
+        "crawling": "true",
+        "selenium": "true",
+        "selenium_inputBoxId": "searchKeyword",
+        "selenium_keyword": "블록체인"
     },
 ]
--- src/main.py
+++ src/main.py
@@ -1,29 +1,61 @@
-import requests
-from bs4 import BeautifulSoup
+# 필요한 라이브러리와 모듈을 임포트합니다.
 import logging
-from .config.websites_config import websites
 import os
 import csv
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+from .config.websites_config import websites
 
+# 로깅 설정을 정의합니다. 로그 메시지는 'log.csv' 파일에 기록됩니다.
 logging.basicConfig(filename='log.csv', level=logging.INFO, format='%(asctime)s,%(message)s')
 
 def read_log():
     logged_links = []
+    expected_format = 4  # 예상하는 형식의 컬럼 수입니다.
     try:
         with open(os.path.join(os.path.dirname(__file__), '..', 'log.csv'), mode='r', newline='', encoding='utf-8') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
                 if row:
-                    link = row[3]
-                    logged_links.append(link)
+                    if len(row) >= expected_format:
+                        link = row[3]  # 4번째 항목(인덱스 3)이 링크입니다.
+                        logged_links.append(link)
+                    else:
+                        # 현재 행의 형식이 예상과 다를 경우, 콘솔에 로깅합니다.
+                        print(f"Unexpected format in log file. Expected {expected_format} columns, but got {len(row)}: {row}")
     except FileNotFoundError:
+        # 파일이 없는 경우 예외 처리
         pass
     return logged_links
 
 def fetch_posts(website):
-    response = requests.get(website['url'])
-    soup = BeautifulSoup(response.content, 'html.parser')
     posts = []
+    if website['selenium'] == "true":
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get(website['url'])
+
+        search_box = driver.find_element(By.ID, website['selenium_inputBoxId'])
+        search_box.send_keys(website['selenium_keyword'] + Keys.RETURN)
+
+        # 페이지가 완전히 로드될 때까지 대기
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, website['selector']))
+        )
+
+        html_source = driver.page_source
+        driver.quit()
+    else:
+        response = requests.get(website['url'])
+        html_source = response.content
+
+    soup = BeautifulSoup(html_source, 'html.parser')
     for row in soup.select(website['selector']):
         title_element = row.select_one('.sbj.txtL a')
         date_element = row.select_one('.date')
@@ -41,8 +73,7 @@ def log_and_print_posts(posts):
         return False
     for post in new_posts:
         message = f"일자: {post['date']}, 제목: {post['title']}, 출처: {post['source']}, 링크: {post['link']}"
-        print(message.replace(',', '\n'))  # 메시지를 콘솔에 출력합니다.
-        # CSV 형식에 맞게 로깅
+        print(message.replace(',', '\n'))
         with open(os.path.join(os.path.dirname(__file__), '..', 'log.csv'), 'a', newline='') as log_file:
             csv_writer = csv.writer(log_file)
             csv_writer.writerow([post['date'], post['title'], post['source'], post['link']])
