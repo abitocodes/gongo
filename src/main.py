920aaf5938e4faf833f329e9bcc55841995d6f82
abitocodes <hancom114@gmail.com>
2024-03-28 19:30:35 +0900

    comparing hashes is operating well. next task is reading keyword search result

--- src/main.py
+++ src/main.py
@@ -13,6 +13,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 # Assume .config.websites_config contains the websites configuration
 from .config.websites_config import websites
+import hashlib
+
+def generate_sha256_hash(text):
+    full_hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
+    return full_hash[:8]  # 해시의 앞 8자리만 반환
 
 # Setup a specific logger for our app
 app_logger = logging.getLogger("AppLogger")
@@ -23,17 +28,16 @@ file_handler.setFormatter(formatter)
 app_logger.addHandler(file_handler)
 
 def read_log():
-    logged_titles = []
+    logged_primaries = []
     try:
         with open('log.csv', mode='r', encoding='utf-8') as csvfile:
             csv_reader = csv.reader(csvfile)
             for row in csv_reader:
-                title = row[3]
-                # print(f"Reading log: {title}")
-                logged_titles.append(title)
+                primary = row[2]  # 해시는 로그의 첫 번째 열에 저장되어 있다고 가정
+                logged_primaries.append(primary)
     except FileNotFoundError:
         pass
-    return logged_titles
+    return logged_primaries
 
 def fetch_posts(website):
     posts = []
@@ -64,22 +68,23 @@ def fetch_posts(website):
     return posts
 
 def log_and_print_posts(posts):
-    logged_titles = read_log()  # 이전에 로그에 기록된 링크들을 읽어옵니다.
-    head_logged_titles = logged_titles[:5]
-    print("head_logged_titles", head_logged_titles)
+    logged_primaries = read_log()  # 이전에 로그에 기록된 해시들을 읽어옵니다.
 
     new_posts = []
     for post in posts:
-        if post['title'] not in logged_titles:
-            new_posts.append(post)  # 기록되지 않은 새로운 포스트만 필터링
+        post_primary = generate_sha256_hash(post['title'])
+        if post_primary not in logged_primaries:
+            post['primary'] = post_primary  # 해시를 포스트 딕셔너리에 추가
+            new_posts.append(post)
     if not new_posts:
         return False
     else:
         for post in new_posts:
-            message = f"{post['date']}, {post['title']}, {post['source']}, {post['link']}"
+            message = f"{post['primary']}, {post['date']}, {post['title']}, {post['source']}, {post['link']}"
             print(message.replace(',', '\n'))  # 콘솔에 출력
             app_logger.info(message)  # 로그 파일에 기록
         return True
+
 
 def main():
     new_posts_found = False
